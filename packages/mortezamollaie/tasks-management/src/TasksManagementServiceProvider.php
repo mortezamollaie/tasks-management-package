093c9f71b56e4e9d50290f5ad3eb691b4805e5e1
mortezamollaie <mm.gov.1381@gmail.com>
2025-03-31 21:48:08 +0330

    feat: initialize tasks management package and configure service provider

---
+++ packages/mortezamollaie/tasks-management/src/TasksManagementServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MortezaMollaie\TasksManagement;
+
+use Illuminate\Support\ServiceProvider;
+
+class TasksManagementServiceProvider extends ServiceProvider
+{
+    public function register()
+
+    {
+
+        //
+
+    }
+
+    public function boot()
+
+    {
+
+        //
+
+    }
+}
